07e6bd1886a2d9f2d754697334f0c79aa327623d
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2021-10-07 19:08:37 +0200

    add new for script

--- removeModels.ts
+++ removeModels.ts
@@ -1,3 +1,29 @@
+// import "colors";
+// import { promises } from "fs";
+// import { join, resolve } from "path";
+// const fsPromises = promises;
+
+// const dirPath = join(process.cwd(), "../swaggerModels/model/");
+// export const removeModels = async (patterns: RegExp[]) => {
+//   await fsPromises.unlink(resolve(`${dirPath}models.ts`));
+//   const fileNames = await fsPromises.readdir(resolve(dirPath));
+//   for (const fileName of fileNames) {
+//     const fileContent = await fsPromises.readFile(dirPath + fileName, "utf8");
+//     for (const pattern of patterns) {
+//       if (pattern.test(fileContent)) {
+//         console.log(`Deleted ${fileName}`.red);
+//         fsPromises.unlink(resolve(dirPath + fileName));
+//         break;
+//       }
+//     }
+//   }
+// };
+
+/* eslint-disable no-plusplus */
+/* eslint-disable no-continue */
+/* eslint-disable no-labels */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-console */
 import "colors";
 import { promises } from "fs";
 import { join, resolve } from "path";
@@ -7,14 +33,26 @@ const dirPath = join(process.cwd(), "../swaggerModels/model/");
 export const removeModels = async (patterns: RegExp[]) => {
   await fsPromises.unlink(resolve(`${dirPath}models.ts`));
   const fileNames = await fsPromises.readdir(resolve(dirPath));
-  for (const fileName of fileNames) {
-    const fileContent = await fsPromises.readFile(dirPath + fileName, "utf8");
+  const array = await Promise.all(
+    await fileNames.map(async (fileName) => {
+      return {
+        fileName: fileName,
+        fileContent: await fsPromises.readFile(dirPath + fileName, "utf8"),
+      };
+    })
+  );
+  const filesToRemove = array.reduce((prev: string[], curr) => {
     for (const pattern of patterns) {
-      if (pattern.test(fileContent)) {
-        console.log(`Deleted ${fileName}`.red);
-        await fsPromises.unlink(resolve(dirPath + fileName));
-        break;
+      if (pattern.test(curr.fileContent)) {
+        return [...prev, curr.fileName];
       }
     }
-  }
+    return [...prev];
+  }, []);
+
+  return await Promise.all(
+    filesToRemove.map((fileName) =>
+      fsPromises.unlink(resolve(dirPath + fileName))
+    )
+  );
 };
